36c7ab0183d18d583355d3e8d6916669681cb737
zzd <zzhdong@gmail.com>
2017-12-27 22:44:27 +0800

    初始化项目

---
+++ src/main/java/com/gimplatform/discovery/DiscoverApplication.java
@@ -0,0 +1,23 @@
+package com.gimplatform.discovery;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务发现程序启动主函数
+ * @author zzd
+ * 注：@SpringBootApplication注解等价于以默认属性使用@Configuration，@EnableAutoConfiguration和@ComponentScan：
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverApplication {
+
+    public static void main(String[] args) {
+    	SpringApplication app = new SpringApplication(DiscoverApplication.class);
+    	app.setBannerMode(Banner.Mode.OFF);
+    	app.run(args); 
+    }
+
+}
