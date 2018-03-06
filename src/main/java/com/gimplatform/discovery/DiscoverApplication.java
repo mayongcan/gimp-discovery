e05d41020b9ad57590b2eeb2c85f4748b5568151
zzhdong <zzhdong@gmail.com>
2018-03-06 11:14:40 +0800

    整理代码格式

--- src/main/java/com/gimplatform/discovery/DiscoverApplication.java
+++ src/main/java/com/gimplatform/discovery/DiscoverApplication.java
@@ -6,18 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * 服务发现程序启动主函数
- * @author zzd
- * 注：@SpringBootApplication注解等价于以默认属性使用@Configuration，@EnableAutoConfiguration和@ComponentScan：
+ * 服务发现程序启动主函数 注：@SpringBootApplication注解等价于以默认属性使用@Configuration，@EnableAutoConfiguration和@ComponentScan：
+ * @author zzd 
  */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoverApplication {
 
     public static void main(String[] args) {
-    	SpringApplication app = new SpringApplication(DiscoverApplication.class);
-    	app.setBannerMode(Banner.Mode.OFF);
-    	app.run(args); 
+        SpringApplication app = new SpringApplication(DiscoverApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
 }
